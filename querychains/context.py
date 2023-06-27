72994fc78b1239a5c61ef7974cea1e90029a0cfb
Ada Bohm <ada@kreatrix.org>
2023-06-27 11:46:41 +0200

    Added locking into Context

--- querychains/context.py
+++ querychains/context.py
@@ -3,6 +3,7 @@ import datetime
 import inspect
 from enum import Enum
 from typing import Any, Callable, Dict, List, Optional
+from threading import Lock
 
 from .data import Data, serialize_with_type
 from .utils import LOG, generate_uid, shorten_str
@@ -70,65 +71,71 @@ class Context:
         self.directory = directory
         self._token = None
         self._depth = 0
+        self._lock = Lock()
 
         if storage:
             storage.register_context(self)
 
     def to_dict(self, with_children=True):
-        result = {"_type": "Context", "name": self.name, "uid": self.uid}
-        if self.state != ContextState.FINISHED:
-            result["state"] = self.state.value
-        for name in ["kind", "inputs", "result", "error", "tags"]:
-            value = getattr(self, name)
-            if value:
-                result[name] = value
-        if with_children and self.children:
-            result["children"] = [c.to_dict() for c in self.children]
-        if not with_children and self.children:
-            result["children_uids"] = [c.uid for c in self.children]
-        if self.start_time:
-            result["start_time"] = self.start_time.isoformat()
-        if self.end_time:
-            result["end_time"] = self.end_time.isoformat()
-        if self.meta:
-            result["meta"] = self.meta
-        return result
+        with self._lock:
+            result = {"_type": "Context", "name": self.name, "uid": self.uid}
+            if self.state != ContextState.FINISHED:
+                result["state"] = self.state.value
+            for name in ["kind", "inputs", "result", "error", "tags"]:
+                value = getattr(self, name)
+                if value:
+                    result[name] = value
+            if with_children and self.children:
+                result["children"] = [c.to_dict() for c in self.children]
+            if not with_children and self.children:
+                result["children_uids"] = [c.uid for c in self.children]
+            if self.start_time:
+                result["start_time"] = self.start_time.isoformat()
+            if self.end_time:
+                result["end_time"] = self.end_time.isoformat()
+            if self.meta:
+                result["meta"] = self.meta
+            return result
 
     @property
     def _pad(self):
         return " " * self._depth
 
     def __enter__(self):
-        assert not self._token
-        assert self.state == ContextState.NEW
-        self.start_time = datetime.datetime.now()
-        parents = _CONTEXT_STACK.get()
-        self._depth = len(parents)
-        if parents:
-            parents[-1].children.append(self)
-        self._token = _CONTEXT_STACK.set(parents + (self,))
-        self.state = ContextState.OPEN
-        LOG.debug(
-            f"{self._pad}Context {self.kind} inputs={shorten_str(self.inputs, 50)}"
-        )
-        return self
+        with self._lock:
+            assert not self._token
+            assert self.state == ContextState.NEW
+            self.start_time = datetime.datetime.now()
+            parents = _CONTEXT_STACK.get()
+            self._depth = len(parents)
+            if parents:
+                parents[-1].children.append(self)
+            self._token = _CONTEXT_STACK.set(parents + (self,))
+            self.state = ContextState.OPEN
+            LOG.debug(
+                f"{self._pad}Context {self.kind} inputs={shorten_str(self.inputs, 50)}"
+            )
+            return self
 
     def __exit__(self, _exc_type, exc_val, _exc_tb):
-        assert self._token
-        assert self.state == ContextState.OPEN
-        if exc_val:
-            self.set_error(exc_val)
-            LOG.debug(
-                f"{self._pad}-> ERR  {self.kind} error={shorten_str(exc_val, 50)}"
-            )
-        else:
-            self.state = ContextState.FINISHED
-            LOG.debug(
-                f"{self._pad}-> OK   {self.kind} result={shorten_str(repr(self.result), 50)}"
-            )
-        self.end_time = datetime.datetime.now()
-        _CONTEXT_STACK.reset(self._token)
-        self._token = None
+        with self._lock:
+            assert self._token
+            assert self.state == ContextState.OPEN
+            if exc_val:
+                # Do not call set_error here as it takes a lock
+                self.state = ContextState.ERROR
+                self.error = serialize_with_type(exc_val)
+                LOG.debug(
+                    f"{self._pad}-> ERR  {self.kind} error={shorten_str(exc_val, 50)}"
+                )
+            else:
+                self.state = ContextState.FINISHED
+                LOG.debug(
+                    f"{self._pad}-> OK   {self.kind} result={shorten_str(repr(self.result), 50)}"
+                )
+            self.end_time = datetime.datetime.now()
+            _CONTEXT_STACK.reset(self._token)
+            self._token = None
         if self.storage:
             self.storage.write_context(self)
         return False  # Propagate any exception
@@ -136,23 +143,27 @@ class Context:
     def add_event(
         self, name: str, kind: Optional[str] = None, data: Optional[Any] = None
     ) -> Event:
-        event = Event(name=name, data=data, kind=kind)
-        self.children.append(event)
-        return event
+        with self._lock:
+            event = Event(name=name, data=data, kind=kind)
+            self.children.append(event)
+            return event
 
     def add_input(self, name: str, value: any):
-        if self.inputs is None:
-            self.inputs = {}
-        if name in self.inputs:
-            raise Exception(f"Input {name} already exists")
-        self.inputs[name] = serialize_with_type(value)
+        with self._lock:
+            if self.inputs is None:
+                self.inputs = {}
+            if name in self.inputs:
+                raise Exception(f"Input {name} already exists")
+            self.inputs[name] = serialize_with_type(value)
 
     def set_result(self, value: any):
-        self.result = serialize_with_type(value)
+        with self._lock:
+            self.result = serialize_with_type(value)
 
     def set_error(self, exc: any):
-        self.state = ContextState.ERROR
-        self.error = serialize_with_type(exc)
+        with self._lock:
+            self.state = ContextState.ERROR
+            self.error = serialize_with_type(exc)
 
 
 def with_context(fn: Callable = None, *, name=None, kind=None):
