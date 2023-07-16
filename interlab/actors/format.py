253a2e11e539ae92a478dcd76a23693aa9668ab9
Tomas Gavenciak <gavento@gmail.com>
2023-07-16 16:56:56 +0200

    Simplify Format object API, improve HTML formatter

--- interlab/actors/format.py
+++ interlab/actors/format.py
@@ -1,74 +1,77 @@
 import html
-import inspect
-from typing import TYPE_CHECKING, Iterable
+from typing import TYPE_CHECKING, Iterable, Any
 
 from .event import Event
 
-if TYPE_CHECKING:
-    from .actor import Actor
-
 
 class FormatBase:
-    def format_event(self, event: Event, observer: "Actor") -> any:
+    def format_event(self, event: Event) -> Any:
         "Format the given Event for the given Actor. To be overriden in subclasses."
         raise NotImplementedError("Use one of the subclasses")
 
-    def format_events(self, events: Iterable[Event], observer: "Actor") -> any:
+    def format_events(self, events: Iterable[Event]) -> Any:
         "Format and join the given Events for the given Actor. To be overriden in subclasses."
         raise NotImplementedError("Use one of the subclasses")
 
-
-class FunFormat(FormatBase):
-    """
-    Generic wrapper for a formatter defined by function (lambda).
-    The function can take 1 or 2 parameters (event, observing_actor).
-    """
-
-    def __init__(self, fun: callable):
-        self.fun = fun
-        self.argnum = len(inspect.signature(self.fun).parameters)
-
-    def format_event(self, event: Event, observer: "Actor") -> any:
-        "Format the given Event for the given Actor using the provided function."
-        if self.argnum == 1:
-            return self.fun(event)
-        return self.fun(event, observer)
+    def __call__(self, ev: Event | Iterable[Event]) -> Any:
+        "Forwarded to self.format_event(ev) or self.format_events(ev)"
+        if isinstance(ev, Event):
+            return self.format_event(ev)
+        elif isinstance(ev, Iterable):
+            return self.format_events(ev)
+        else:
+            raise TypeError(
+                f"only Event or iterable of events can be formatted, got {ev.__class__}"
+            )
 
 
 class TextFormat(FormatBase):
     "Abstract base for plaintext-based formatters"
     JOIN_STR = "\n\n"
 
-    def format_events(self, events: Iterable[Event], observer: "Actor") -> any:
+    def format_events(self, events: Iterable[Event]) -> any:
         "Format events and join them with `JOIN_STR` (\\n\\n by default)."
-        return self.JOIN_STR.join(self.format_event(e, observer) for e in events)
+        return self.JOIN_STR.join(self.format_event(e) for e in events)
 
 
 class LLMTextFormat(TextFormat):
     "Default plaintext-based formatter for LLMs"
 
-    def format_event(self, event: Event, observer: "Actor") -> str:
+    def format_event(self, event: Event) -> str:
         s = event.data_as_string()
-        if event.origin is not None:
-            return f"{event.origin}: {s}"
-        return s
+        return f"{event.origin}: {s}" if event.origin is not None else s
 
 
 class HumanTextFormat(TextFormat):
     "Default plaintext-based formatter for human actors and observers"
 
-    def format_event(self, event: Event, observer: "Actor") -> str:
-        s = event.data_as_string(json_indent=2)
-        if event.origin is not None:
-            return f"{event.origin}:\n{s}"
-        return s
+    def format_event(self, event: Event) -> str:
+        s = event.data_as_string()
+        # If the answer is a longer JSON, display over multiple lines
+        if len(s) > 60 and not isinstance(event.data, str):
+            s = event.data_as_string(json_indent=2)
+        return f"{event.origin}: {s}" if event.origin is not None else s
 
 
 class HTMLFormat(TextFormat):
+    CLASS_NAME_PREFIX = "InterLabHTMLFormat"
     "Default HTML-based formatter for human actors and observers"
 
-    def format_event(self, event: Event, observer: "Actor") -> str:
-        s = f"<code>{html.escape(event.data_as_string(json_indent=2))}</code>"
+    def format_events(self, events: Iterable[Event]) -> any:
+        s = "\n".join(self.format_event(e) for e in events)
+        return f'<div class="{self.CLASS_NAME_PREFIX}__box">\n{s}\n</div>'
+
+    def format_event(self, event: Event) -> str:
+        s = event.data_as_string()
+
+        if isinstance(event.data, str):
+            fs = f'<span class="{self.CLASS_NAME_PREFIX}__text">{html.escape(s)}</span>'
+        else:
+            # If the answer is a longer JSON, display over multiple lines
+            if len(s) > 60:
+                s = event.data_as_string(json_indent=2)
+            fs = f'<code class="{self.CLASS_NAME_PREFIX}__json">{html.escape(s)}</code>'
+
         if event.origin is not None:
-            return f"<span>{html.escape(event.origin)}:</span> {s}"
-        return s
+            fs = f'<span class="{self.CLASS_NAME_PREFIX}__origin">{html.escape(event.origin)}:</span> {fs}'
+        return f'<div class="{self.CLASS_NAME_PREFIX}__message">{fs}:</div>'
