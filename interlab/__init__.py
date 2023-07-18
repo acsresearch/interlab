0124a1dd1bf42fddb57a609db5668ca617def831
Tomas Gavenciak <gavento@gmail.com>
2023-07-18 19:50:53 +0200

    Cleanup, root reexports, notebooks outdated and updated (WIP)

--- interlab/__init__.py
+++ interlab/__init__.py
@@ -1,5 +1,5 @@
 from . import actors, context, llm, utils  # noqa: F401
-from .context import Context, with_context  # noqa: F401
 
 # Removed for minimality, may be re-added later
 # from .context.storage import FileStorage  # noqa: F401
+# from .context import Context, with_context  # noqa: F401
