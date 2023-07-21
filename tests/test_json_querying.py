cdee9219c96a43a922a68dee56d61af4a2bb2784
Tomas Gavenciak <gavento@gmail.com>
2023-07-22 00:00:04 +0200

    Make query_for_json robust to prompt strings with "{..}"

--- tests/test_json_querying.py
+++ tests/test_json_querying.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import pytest
+
 from interlab.llm import json_querying
 
 
@@ -14,6 +16,13 @@ def test_query_for_json():
     def eng(q):
         if "TEST_A" in q:
             return "heh {'z': \"0\"} zzz"
+        if "TEST_B" in q:
+            return "```{'z': \"0\", 'x': 33}``` {'z': 1}"
         raise Exception()
 
-    assert json_querying.query_for_json(eng, Foo, "TEST_A")
+    assert json_querying.query_for_json(eng, Foo, "TEST_A") == Foo(z=False)
+    assert json_querying.query_for_json(
+        eng, Foo, "{absent} TEST_B {FORMAT_PROMPT} zzz"
+    ) == Foo(z=False, x=33)
+    with pytest.raises(ValueError):
+        json_querying.query_for_json(eng, Foo, "{FORMAT_PROMPT} TEST_A {FORMAT_PROMPT}")
