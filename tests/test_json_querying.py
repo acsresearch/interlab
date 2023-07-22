1301fba8ae86fb80b3ef121f8dda0de901fb5415
Tomas Gavenciak <gavento@gmail.com>
2023-07-22 02:00:14 +0200

    Minor fixes to json quarying, test

--- tests/test_json_querying.py
+++ tests/test_json_querying.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 
 import pytest
@@ -7,9 +8,12 @@ from interlab.llm import json_querying
 
 @dataclass
 class Foo:
-    z: bool
+    z: bool = True
     x: int = 0
     y: list[str] | None = None
+
+
+FOO_SCHEMA_RE = r"```json\s*\n\s*{'type':\s*'object',\s'properties':\s{'z':"
 
 
 def test_query_for_json():
@@ -18,6 +22,9 @@ def test_query_for_json():
             return "heh {'z': \"0\"} zzz"
         if "TEST_B" in q:
             return "```{'z': \"0\", 'x': 33}``` {'z': 1}"
+        if "TEST_C" in q:
+            assert re.search(FOO_SCHEMA_RE, q, re.MULTILINE)
+            return "{'x':3}"
         raise Exception()
 
     assert json_querying.query_for_json(eng, Foo, "TEST_A") == Foo(z=False)
@@ -26,3 +33,5 @@ def test_query_for_json():
     ) == Foo(z=False, x=33)
     with pytest.raises(ValueError):
         json_querying.query_for_json(eng, Foo, "{FORMAT_PROMPT} TEST_A {FORMAT_PROMPT}")
+    json_querying.query_for_json(eng, Foo, "zzz {FORMAT_PROMPT}TEST_C") == Foo(x=3)
+    json_querying.query_for_json(eng, Foo, "TEST_C") == Foo(x=3)
