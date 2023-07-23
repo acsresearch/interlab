dd425ef9909c1ea509a55f711d75d12868fce930
Tomas Gavenciak <gavento@gmail.com>
2023-07-23 23:45:50 +0200

    Rename class to FormatStr, fix formatting and lints

--- tests/test_format_str.py
+++ tests/test_format_str.py
@@ -1,9 +1,9 @@
-from interlab.llm import fmtstring
+from interlab.llm import format_str
 
 
 def test_fmtstring():
     S = "as{xyz}{{df{x:.3f}"
-    f = fmtstring.FmtString(S)
+    f = format_str.FormatStr(S)
     assert str(f) == S
     assert str(f.format()) == str(f).replace("{{", "{")
     assert str(f.format(xyz="X")) == "asX{df{x:.3f}"
@@ -11,7 +11,7 @@ def test_fmtstring():
     assert f.free_params() == {"xyz", "x"}
 
     S2 = "TST {aa} {x} {aa}"
-    f2 = fmtstring.FmtString(S2)
+    f2 = format_str.FormatStr(S2)
     assert f2.free_params() == {"aa", "x"}
     f2b = f2.format(aa=f.format(xyz="C"))
     assert str(f2b) == "TST asC{df{x:.3f} {x} asC{df{x:.3f}"
@@ -21,4 +21,3 @@ def test_fmtstring():
     assert str(f2b.format(x=42, _recursive=True)) == "TST asC{df42.000 42 asC{df42.000"
 
     assert f2b.into_html().startswith("<span")
-
