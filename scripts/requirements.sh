0ff1789d5c5854a6454034bff9658aae5ae33c4c
Tomas Gavenciak <gavento@gmail.com>
2023-07-25 19:59:29 +0200

    Improve the requirement script logs, update CI poetry to 1.5.1

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -14,20 +14,29 @@ echo "### Interlab root dir $ROOT"
 echo "### Using temp dir $TEMP"
 cd "$TEMP"
 
+echo
+echo "### Running poetry version:"
+poetry --version
+echo "### Note: Github CI is using Poetry version:"
+grep 'key: "\?poetry-[0-9\.]\+"\?$' "$ROOT/.github/workflows/test.yaml"
+
 # Update default (full) requirements
+echo
 echo "### Writing standard requirements.txt"
 cp "$ROOT/pyproject.toml" "$ROOT/poetry.lock" .
 poetry lock --check
 poetry export -o "requirements.txt" --without-hashes --without-urls
 
 # Update requirements for google colab
+echo
 echo "### Writing tweaked requirements.txt for google colab"
 # First, remove the "notebooks" group
 sed 'H;1h;$!d;x;s/\[tool.poetry.group.notebooks.dependencies\][^[]*//' -i pyproject.toml
 # google-colab specific versions
 poetry add --lock "requests==2.27.1" "numpy==1.22.4" "decorator==4.4.2"
-poetry export -o "requirements-colab.txt" --without notebooks --without dev --without-hashes --without-urls
+poetry export -o "requirements-colab.txt" --without dev --without-hashes --without-urls
 
+echo
 if [ "$1" == "--check" ]; then
     echo "### Checking requirement files being up-to-date ..."
     diff "$ROOT/requirements.txt" ./requirements.txt
@@ -37,6 +46,6 @@ else
     cp ./requirements.txt ./requirements-colab.txt "$ROOT"
 fi
 
-echo "Finished, cleaning up"
+echo "### Finished, cleaning up"
 cd "$ROOT"
 rm -r "$TEMP"
