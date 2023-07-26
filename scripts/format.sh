2cc753be66dbde6d03c994974a3816be0a5c277b
Tomas Gavenciak <gavento@gmail.com>
2023-07-26 23:30:26 +0200

    Unify format+check scripts and add config var, update CI

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Formats and lints files in-place
+# Formats and lints files in-place, or just checks them for formatting
+
+DIRS="tests interlab interlab_zoo"
 
 set -e
 
+if [[ "$1" != "--check" && "x$1" != "x" ]]; then
+    echo "Run as '$0 --check' or just '$0'"
+    exit 1
+fi
+
 cd `dirname $0`/..
 
-# Format Python code
-poetry run isort --profile black tests interlab interlab_zoo
-poetry run black tests interlab interlab_zoo
+if [[ "$1" == "--check" ]]; then
+    poetry run isort --check-only --profile black $DIRS
+    poetry run black --check $DIRS
+    # Lint Python code
+    # By default, this is skipped in checking
+    #poetry run flake8 tests interlab
+    echo "All code checks completed successfully"
+else
+    # Format Python code
+    poetry run isort --profile black $DIRS
+    poetry run black $DIRS
+    # Lint Python code
+    poetry run flake8 $DIRS
+fi
 
-# Lint Python code
-poetry run flake8 tests interlab
