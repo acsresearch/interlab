a8c96f164922bb417df230bd6f49ea4d100b2406
Tomas Gavenciak <gavento@gmail.com>
2023-07-27 17:22:31 +0200

    Use QueryFailure more consistently in queries

--- interlab/queries/query_failure.py
+++ interlab/queries/query_failure.py
@@ -1,2 +1,17 @@
 class QueryFailure(Exception):
+    """
+    Indicates a general failure related to the result of a model query.
+
+    This mostly means that the model failed to supply parseable or othervise proper output,
+    or that the subcomputation failed in some other way.
+
+    Note that this should not include cases like network errors (e.g. model unavailability).
+    """
+
+    pass
+
+
+class ParsingFailure(Exception):
+    """Specific QueryFailure indicating that the model output could not be parsed."""
+
     pass
