28618b83d2614f31734c4254972fc33c83652ecb
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-07-27 11:51:21 +0200

    Print absolute path to logs storage directory

--- experiments/jason/set_storage.py
+++ experiments/jason/set_storage.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from interlab import context
 
 if __name__ == '__main__':
     storage = context.FileStorage("logs")  # Directory for storing contexts (structured logs)
+    print(Path(storage.directory).absolute())
     server = storage.start_server()
     print(f"{server.url=}")
