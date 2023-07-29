a4a70d21124d63344d5ea623ed5f476c650b9980
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-07-29 16:48:11 +0200

    Allow specifying the root directory in set_storage.py

--- interlab_zoo/jason/set_storage.py
+++ interlab_zoo/jason/set_storage.py
@@ -1,9 +1,19 @@
+import argparse
 from pathlib import Path
 
 from interlab import context
 
-if __name__ == '__main__':
-    storage = context.FileStorage("logs")  # Directory for storing contexts (structured logs)
+
+def main():
+    # arg-parse Directory for storing contexts (structured logs)
+    args = argparse.ArgumentParser()
+    args.add_argument(dest="storage", type=Path)
+    args = args.parse_args()
+    storage = context.FileStorage(args.storage)
     print(Path(storage.directory).absolute())
     server = storage.start_server()
     print(f"{server.url=}")
+
+
+if __name__ == '__main__':
+    main()
