8b91e44fa5de7ed776eb462382566648e410b6fe
Tomas Gavenciak <gavento@gmail.com>
2023-07-30 23:44:07 +0200

    Make requested word count in summary dynamic
    Seems to work fine, usually getting twice as many tokens as requested
    words from GPT-3.5

--- interlab/queries/summarize.py
+++ interlab/queries/summarize.py
@@ -7,17 +7,20 @@ from ..lang_models import count_tokens, query_model
 
 
 def summarize_with_limit(
-    text: str, model: Any, token_limit: int = 300, length_request: str = "100 words"
+    text: str, model: Any, token_limit: int = 300, length_request: str = None
 ):
     """Summarize text to within a token limit.
 
     Always summarizes even a shorter text (for style consistency).
     Iterates at most twice, then truncates the last summary by token count with " ..." added.
-    If you change the token limit, you should also change the length request.
-    Note that "100 words" usually produces a summary of 100-150 tokens with GPT-3.
+
+    Lenght request is e.g. "100 words" or "one paragraph" etc. By default, it is
+    `f"{token_limit // } words"`.
     """
     if token_limit < 10:
         raise ValueError("Token limit must be at least 10.")
+    if length_request is None:
+        length_request = f"{token_limit // 2} words"
     # TODO: add a (large) token limit to the call below when query_model supports it
     summary = query_model(
         model,
