8be83784282f1cab05f25ad78e74cb2890a011b4
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-07-31 12:23:25 +0200

    Display successful attacks in results_browser.py

--- interlab_zoo/jason/adversarial_prompting/results_browser.py
+++ interlab_zoo/jason/adversarial_prompting/results_browser.py
@@ -44,11 +44,12 @@ with st.expander("Results dataframe"):
     st.write(df)
 
 with st.expander("Informativeness trajectories"):
+    df_for_plot = df.copy()
     # plot informativeness_history for each successful experiment using plotly
     for col in ["response", "experiment"]:
-        df[col] = df[col].apply(lambda s: "<br>".join(wrap(s, 100)))
+        df_for_plot[col] = df_for_plot[col].apply(lambda s: "<br>".join(wrap(s, 100)))
 
-    for model, model_df in df.groupby("victim.model"):
+    for model, model_df in df_for_plot.groupby("victim.model"):
         fig = px.strip(  # allows to use jitter to avoid overlapping points
             model_df,
             x="round",
@@ -68,3 +69,36 @@ with st.expander("Informativeness trajectories"):
         fig.update_layout(showlegend=False)
         fig.update_xaxes(range=[-0.2, 9.2])
         st.plotly_chart(fig, use_container_width=True)
+
+# st.write(df)
+# with st.expander("Display successful runs"):
+list_agg_cols = [
+    col
+    for col in df.columns
+    if not any(col.startswith(role) for role in ["attacker", "victim", "judge"])
+]
+for col in ["cache", "rounds", "experiment"]:
+    list_agg_cols.remove(col)
+first_agg_cols = [col for col in df.columns if col not in list_agg_cols]
+df_success = df.groupby("context_id").agg(
+    {
+        **{col: list for col in list_agg_cols},
+        **{col: "first" for col in first_agg_cols},
+    }
+)
+df_success = df_success[
+    df_success.success.apply(lambda success_values: any(success_values))
+]
+# st.write(df_success)
+
+for victim_model, df_success_victim in df_success.groupby("victim.model"):
+    with st.expander(f"Display successful attacks on model: {victim_model}"):
+        for _, row in df_success_victim.iterrows():
+            st.write("experiment", row["experiment"])
+            # interleave prompt and response and format them differently
+            for informativeness, prompt, response in zip(
+                row["informativeness"], row["prompt"], row["response"]
+            ):
+                st.write(f":red[{prompt}]")
+                # st.write(f":blue[{response}]")  # breaks because response contains square brackets
+                st.write(f":blue[{informativeness}] {response}")
