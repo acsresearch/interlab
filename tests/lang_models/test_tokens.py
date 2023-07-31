7a2164e742470128fb2d3e6996aa9cc85700aedf
Tomas Gavenciak <gavento@gmail.com>
2023-07-31 04:25:44 +0200

    Fix test when there are not API keys given

--- tests/lang_models/test_tokens.py
+++ tests/lang_models/test_tokens.py
@@ -12,15 +12,38 @@ TEXT = (
 
 def test_count_tokens():
     assert count_tokens(TEXT, "gpt2") == 56
-    assert count_tokens(TEXT, "gpt-4") == 40
+    assert count_tokens(TEXT, "text-curie-001") == 56
     assert count_tokens(TEXT, "claude-2") == 57
     with pytest.raises(ValueError):
         count_tokens(TEXT, "foobar")
-    assert count_tokens(TEXT, interlab.lang_models.AnthropicModel()) == 57
-    assert count_tokens(TEXT, interlab.lang_models.OpenAiChatModel("gpt-4")) == 40
-    assert count_tokens(TEXT, langchain.chat_models.ChatOpenAI(model="gpt-4")) == 40
-    assert count_tokens(TEXT, langchain.chat_models.ChatOpenAI()) == 40
-    assert count_tokens(TEXT, langchain.chat_models.ChatAnthropic()) == 57
-    assert count_tokens(TEXT, langchain.OpenAI(model="babbage")) == 56
-    assert count_tokens(TEXT, langchain.OpenAI(model="babbage")) == 56
+    assert count_tokens(TEXT, interlab.lang_models.AnthropicModel(api_key="Foo")) == 57
+    print(str(interlab.lang_models.OpenAiChatModel(model="gpt-4", api_key="Foo")))
+    assert (
+        count_tokens(
+            TEXT, interlab.lang_models.OpenAiChatModel(model="gpt-4", api_key="Foo")
+        )
+        == 40
+    )
+    assert (
+        count_tokens(
+            TEXT, langchain.chat_models.ChatOpenAI(model="gpt-4", openai_api_key="Foo")
+        )
+        == 40
+    )
+    assert (
+        count_tokens(TEXT, langchain.chat_models.ChatOpenAI(openai_api_key="Foo")) == 40
+    )
+    assert (
+        count_tokens(TEXT, langchain.chat_models.ChatAnthropic(anthropic_api_key="Foo"))
+        == 57
+    )
+    assert (
+        count_tokens(TEXT, langchain.OpenAI(model="babbage", openai_api_key="Foo"))
+        == 56
+    )
+    assert count_tokens.cache_info().hits == 0
+    assert (
+        count_tokens(TEXT, langchain.OpenAI(model="babbage", openai_api_key="Foo"))
+        == 56
+    )
     assert count_tokens.cache_info().hits == 1
