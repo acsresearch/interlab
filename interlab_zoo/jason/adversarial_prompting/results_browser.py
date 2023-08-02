791ba80e3abf9c7d895110c09e3ef15a3b1249ef
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-08-02 12:38:45 +0200

    Load error information for display in results_browser.py

--- interlab_zoo/jason/adversarial_prompting/results_browser.py
+++ interlab_zoo/jason/adversarial_prompting/results_browser.py
@@ -10,9 +10,11 @@ from textwrap import wrap
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import yaml
 from jinja2 import Template
 from streamlit.components.v1 import html
 
+from interlab.context import FileStorage, Context
 from interlab_zoo.jason.adversarial_prompting import utils
 
 # wide layout
@@ -27,6 +29,28 @@ experiment_folder = st.text_input(
 read_result_json = st.cache_data(utils.read_result_json)
 
 
+def read_contexts(experiment_folder: Path | str) -> dict[str, Context]:
+    storage = FileStorage(experiment_folder)
+    contexts = storage.read_all_contexts()
+    return {context.uid: context for context in contexts}
+
+
+def read_hydra_configs(experiment_folder: Path | str) -> dict[str, dict]:
+    configs = {}
+    for subfolder in Path(experiment_folder).iterdir():
+        if subfolder.is_dir():
+            config_file = subfolder / ".hydra" / "config.yaml"
+            with open(config_file) as f:
+                config = yaml.safe_load(f)
+
+            # hack in the context id
+            context_id = next(subfolder.glob("*.root.gz")).name[: -len(".root.gz")]
+            config["context_id"] = context_id
+
+            configs[subfolder.name] = config
+    return configs
+
+
 def read_results(experiment_folder: Path | str) -> pd.DataFrame:
     subfolders = (f for f in Path(experiment_folder).iterdir() if f.is_dir())
     results = {
@@ -34,13 +58,67 @@ def read_results(experiment_folder: Path | str) -> pd.DataFrame:
         for subfolder in subfolders
         if (subfolder / "result.json").exists()
     }
-    return pd.concat(results).reset_index(drop=True)
+    results_df = pd.concat(results).reset_index(drop=True)
+
+    # read all hydra configs to also catch runs that failed before writing results.json
+    # todo: just store all information in the context and take it from there for consistency
+    folder_to_config = read_hydra_configs(experiment_folder)
+    for folder, config in folder_to_config.items():
+        if folder in results_df.experiment.unique():
+            continue
+        # else append a new row
+        results_df = pd.concat(
+            [
+                results_df,
+                pd.json_normalize(config).assign(experiment=folder),
+            ],
+            ignore_index=True,
+        )
+
+    # add info about the experiment state ("finished", "error")
+    uid_to_context = read_contexts(experiment_folder)
+    results_df["state"] = results_df["context_id"].apply(
+        lambda x: uid_to_context[x].state.value
+    )
+
+    # error:{
+    # _type:"error",
+    # name:"This model's maximum context length is 4097 tokens. However, your messages resulted in 4236 tokens. Please reduce the length of the messages."
+    # }
+    def get_error_name(context_id: str) -> str:
+        context = uid_to_context[context_id]
+        if context.error is None:
+            return ""
+        return context.error["name"]
+
+    results_df["error"] = results_df["context_id"].apply(get_error_name)
+    return results_df
 
 
 df = read_results(experiment_folder)
+# st.write(df.groupby("experiment").agg(set).reset_index())
 
 with st.expander("Results dataframe"):
     st.write(f"Total number of experiments: {df.experiment.nunique()}")
+    # statistics of "state" per "victim.model"
+    st.write(
+        df.groupby("experiment")
+        .agg("first")
+        .groupby(["victim.model", "state"])
+        .size()
+        .unstack(fill_value=0)
+    )
+    # statistics of "error" per "victim.model"
+    df_error_short = df.copy()
+    df_error_short["error"] = df_error_short["error"].apply(lambda x: x[:70])
+    st.write(
+        df_error_short.groupby("experiment")
+        .agg("first")
+        .groupby(["victim.model", "error"])
+        .size()
+        .unstack(fill_value=0)
+    )
+
     num_exp_per_victim = df.groupby("victim.model").experiment.nunique()
     num_success_per_victim = df.groupby("victim.model").sum()["success"]
     # join the two series into a dataframe
@@ -57,7 +135,9 @@ with st.expander("Informativeness trajectories"):
     df_for_plot = df.copy()
     # plot informativeness_history for each successful experiment using plotly
     for col in ["response", "experiment"]:
-        df_for_plot[col] = df_for_plot[col].apply(lambda s: "<br>".join(wrap(s, 100)))
+        df_for_plot[col] = df_for_plot[col].apply(
+            lambda s: "<br>".join(wrap(s, 100)) if isinstance(s, str) else s
+        )
 
     for model, model_df in df_for_plot.groupby("victim.model"):
         fig = px.strip(  # allows to use jitter to avoid overlapping points
