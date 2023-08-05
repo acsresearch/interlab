d2d60dad36ac79929c104c5b95bebfd07da39838
Ada Bohm <ada@kreatrix.org>
2023-08-05 11:38:04 +0200

    Fix inconsisteny logging in interlab models and query_model

--- tests/lang_models/test_model.py
+++ tests/lang_models/test_model.py
@@ -4,8 +4,10 @@ from typing import Type
 
 import pytest
 
+from interlab.context import Context
 from interlab.context.serialization import serialize_with_type
 from interlab.lang_models import AnthropicModel, LangModelBase, OpenAiChatModel
+from tests.testutils import strip_tree
 
 
 def test_serialize_models():
@@ -35,12 +37,39 @@ def test_serialize_models():
     not all(os.getenv(key) for key in ["OPENAI_API_KEY", "ANTHROPIC_API_KEY"]),
     reason="Requires API keys",
 )
-@pytest.mark.parametrize("model", [AnthropicModel, OpenAiChatModel])
-def test_query(model: Type[LangModelBase]):
-    model = model()
-    output = model.query("Hello", max_tokens=10)
-    assert isinstance(output, str)
-    assert output
+@pytest.mark.parametrize("model_cls", [AnthropicModel, OpenAiChatModel])
+def test_query(model_cls: Type[LangModelBase]):
+    model = model_cls()
+    with Context("my_query") as c:
+        output = model.query("Hello", max_tokens=10)
+        assert isinstance(output, str)
+        assert output
+
+    output = strip_tree(c.to_dict())
+    name = model_cls.__module__ + "." + model_cls.__qualname__
+    assert isinstance(output["children"][0].pop("result"), str)
+    print(output)
+    assert output == {
+        "_type": "Context",
+        "name": "my_query",
+        "children": [
+            {
+                "_type": "Context",
+                "name": f"query interlab {model_cls.__qualname__} ({model.model})",
+                "kind": "query",
+                "inputs": {
+                    "prompt": "Hello",
+                    "conf": {
+                        "class": name,
+                        "model_name": model.model,
+                        "temperature": model.temperature,
+                        "max_tokens": 10,
+                        "strip": True,
+                    },
+                },
+            }
+        ],
+    }
 
 
 @pytest.mark.skipif(
