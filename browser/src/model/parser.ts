b95d54017a6014e414024a200173ea5dc8015d95
Ada Bohm <ada@kreatrix.org>
2023-08-24 11:04:33 +0200

    WIP

--- browser/src/model/parser.ts
+++ browser/src/model/parser.ts
@@ -1,24 +1,55 @@
 
 export enum TokenType {
-    Ident,
-    Operator,
-    Literal,
-    LiteralNum,
+    Ident = "ident",
+    Operator = "op",
+    LiteralStr = "literal_s",
+    LiteralNum = "literal_n",
 }
 
 export type Token = {
     type: TokenType,
     index: number,
-    value: string,
+    value: string | number,
 }
 
 //export type Token = TokenIdent | TokenOperator | TokenLiteral;
 
-const TOKEN_REGEXP = /([\w.]+)|(\d+)|([=<>]+)|\"(?:\\\"|[^\"])*\"|./g
+const TOKEN_REGEXP = /(?<ident>[A-Za-z][\w.]*)|(?<int>\d+)|(?<op>[=<>]+)|\"(?<str>(?:\\\"|[^\"]))*\"|(?<space>\s+)|./g
 
-function parse_tokens(input: string): Token[] {
+export function parse_tokens(input: string): Token[] {
+    const tokens: Token[] = [];
     for (const match of input.matchAll(TOKEN_REGEXP)) {
-        const value = match[0]
-        const index = match.index;
+        if (match.groups.space) {
+            continue
+        }
+        let value = match[0]
+        let token_type: TokenType | undefined;
+        if (match.groups.ident) {
+            token_type = TokenType.Ident;
+        } else if (match.groups.op) {
+            token_type = TokenType.Operator;
+        } else if (match.groups.int) {
+            token_type = TokenType.LiteralNum;
+            value = +value;
+        } else if (match.groups.str) {
+            token_type = TokenType.LiteralStr;
+        } else {
+            throw new Error("Invalid token");
+        }
+        tokens.push({ type: token_type, value, index: match.index })
+        // console.count(`xxx <${first}>`)
+        // if (first.match(/\s/)) {
+        //     continue
+        // }
+        // if (first.match(/[A-Za-z]/)) {
+        //     tokens.push({ type: TokenType.Ident, value, index })
+        //     continue
+        // }
+        // if (first.match(/[A-Za-z]/)) {
+        //     tokens.push({ type: TokenType.Ident, value, index })
+        //     continue
+        // }
+
     }
+    return tokens;
 }
--- browser/src/tests/parser.test.tsx
+++ browser/src/tests/parser.test.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { parse_tokens } from '../model/parser';
 
 
-const TOKEN_REGEXP = /([\w.]+)|(\d+)|([=<>]+)|\"(?:\\\"|[^\"])*\"|./g
-//const TOKEN_REGEXP = /[\w.]+|\d+|[=<>]+|\"(?:\\\"|[^\"]*\")/
+// const TOKEN_REGEXP = /([\w.]+)|(\d+)|([=<>]+)|\"(?:\\\"|[^\"])*\"|./g
+// //const TOKEN_REGEXP = /[\w.]+|\d+|[=<>]+|\"(?:\\\"|[^\"]*\")/
 
-test('renders learn react link', () => {
-    //console.log("a.bbb= \"xxx\" < 1".match(TOKEN_REGEXP))
-    const s = "a.bbb= \"xxx\" < $ 1 ";
+// test('renders learn react link', () => {
+//     //console.log("a.bbb= \"xxx\" < 1".match(TOKEN_REGEXP))
+//     const s = "a.bbb= \"xxx\" < $ 1 ";
 
-    //console.log(TOKEN_REGEXP.exec(s));
+//     //console.log(TOKEN_REGEXP.exec(s));
 
-    for (const result of s.matchAll(TOKEN_REGEXP)) {
-        console.log(">>>", result)
-    }
+//     for (const result of s.matchAll(TOKEN_REGEXP)) {
+//         console.log(">>>", result)
+//     }
+// });
+
+
+test('test tokens', () => {
+    expect(parse_tokens("abc.xyz = 123")).toStrictEqual([])
 });
