8eff1c3912cdf0f1cf00878e6d28d49f63c9ce73
Ada Bohm <ada@kreatrix.org>
2023-10-04 18:05:00 +0200

    pyplot extension allows closing figures

--- interlab/ext/pyplot.py
+++ interlab/ext/pyplot.py
@@ -1,7 +1,7 @@
 from ..context.data.blob import DataWithMime
 
 
-def capture_figure(file_format: str = "png", **kwargs):
+def capture_figure(figure=None, file_format: str = "png", **kwargs):
     import io
 
     import matplotlib.pyplot as plt
@@ -9,6 +9,8 @@ def capture_figure(file_format: str = "png", **kwargs):
     buf = io.BytesIO()
     plt.savefig(buf, format=file_format, **kwargs)
     buf.seek(0)
+    if figure:
+        plt.close(figure)
     if file_format == "png":
         mime = "image/png"
     else:
