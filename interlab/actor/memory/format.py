b9313a5b3fda32f7e6bbcf49852993a269aa85d9
Ada Bohm <ada@kreatrix.org>
2023-11-13 12:15:00 +0100

    Text formatters renaming

--- interlab/actor/memory/format.py
+++ interlab/actor/memory/format.py
@@ -1,4 +1,5 @@
 import abc
+from abc import ABC
 from typing import Any, Iterable
 
 from ..event import Event
@@ -7,16 +8,16 @@ from ..event import Event
 class FormatBase(abc.ABC):
     @abc.abstractmethod
     def format_event(self, event: Event) -> Any:
-        "Format the given Event for the given Actor. To be overriden in subclasses."
+        """Format the given Event for the given Actor. To be overriden in subclasses."""
         raise NotImplementedError("Use one of the subclasses")
 
     @abc.abstractmethod
     def format_events(self, events: Iterable[Event]) -> Any:
-        "Format and join the given Events for the given Actor. To be overriden in subclasses."
+        """Format and join the given Events for the given Actor. To be overriden in subclasses."""
         raise NotImplementedError("Use one of the subclasses")
 
     def __call__(self, ev: Event | Iterable[Event]) -> Any:
-        "Forwarded to self.format_event(ev) or self.format_events(ev)"
+        """Forwarded to self.format_event(ev) or self.format_events(ev)"""
         if isinstance(ev, Event):
             return self.format_event(ev)
         elif isinstance(ev, Iterable):
@@ -27,25 +28,26 @@ class FormatBase(abc.ABC):
             )
 
 
-class TextFormat(FormatBase):
-    "Abstract base for plaintext-based formatters"
+class TextFormat(FormatBase, ABC):
+    """Abstract base for plaintext-based formatters"""
+
     JOIN_STR = "\n\n"
 
     def format_events(self, events: Iterable[Event]) -> Any:
-        "Format events and join them with `JOIN_STR` (\\n\\n by default)."
+        """Format events and join them with `JOIN_STR` (\\n\\n by default)."""
         return self.JOIN_STR.join(self.format_event(e) for e in events)
 
 
-class LLMTextFormat(TextFormat):
-    "Default plaintext-based formatter for LLMs"
+class DefaultTextFormat(TextFormat):
+    """Default plaintext-based formatter"""
 
     def format_event(self, event: Event) -> str:
         s = event.data_as_string()
         return f"{event.origin}: {s}" if event.origin is not None else s
 
 
-class HumanTextFormat(TextFormat):
-    "Default plaintext-based formatter for human actors and observers"
+class JsonFormattingTextFormat(TextFormat):
+    """Default plaintext-based formatter for human actors and observers"""
 
     def format_event(self, event: Event) -> str:
         s = event.data_as_string()
