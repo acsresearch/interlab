426048436c727c3e8e4b6369cb082b6350555976
Ada Bohm <ada@kreatrix.org>
2024-01-15 17:03:49 +0100

    CDN link updated

--- treetrace/ui/staticview.py
+++ treetrace/ui/staticview.py
@@ -5,7 +5,7 @@ from ..tracing.tracingnode import TracingNode
 from .staticfiles import get_current_js_and_css_filenames
 
 JS_FILE, CSS_FILE = get_current_js_and_css_filenames()
-CDN_VERSION = "5464935062082e96e24a95cda009e5419ce09a51"  # In future, it should match the released version
+CDN_VERSION = "9fbc2169cd64cac537b74c432a6d1a7a2b7e8b0c"
 CDN_URL = f"https://cdn.jsdelivr.net/gh/acsresearch/interlab@{CDN_VERSION}/treetrace/ui/browser/assets/"
 
 # ONLY FOR LOCAL TESTING
