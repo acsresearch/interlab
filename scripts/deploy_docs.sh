25e120b530495edd160dfb8a2004f9347697e612
Tomas Gavenciak <gavento@gmail.com>
2024-01-20 19:25:39 -0800

    Fix deploy script (dummy)

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -5,8 +5,9 @@ set -e
 cd `dirname $0`/..
 
 #poetry run mkdocs gh-deploy
-echo "For now, run `mike deploy` manually *after* running `scripts/gen_doc.sh`"
-echo "- `poetry run mike deploy -u -p latest` for latest docs (`main` branch)"
-echo "- `poetry run mike deploy -u -p --no-redirect 0.4.0 stable` for stable versions (replace 0.4.0)"
+echo "For now, run 'mike deploy' manually *after* running 'scripts/gen_doc.sh'"
+echo "- 'poetry run mike deploy -u -p latest' for latest docs ('main' branch)"
+echo "- 'poetry run mike deploy -u -p --no-redirect 0.4.0 stable' for stable versions (replace 0.4.0)"
 echo 
 echo "NB: nothing was done"
+exit 1
