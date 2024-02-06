f202bf037762e71138ca25591d4450826ad53b4e
Ada Bohm <ada@kreatrix.org>
2024-02-06 15:48:58 +0100

    WIP

--- interlab_zoo/persona/channels.py
+++ interlab_zoo/persona/channels.py
@@ -5,23 +5,35 @@ from dataclasses import dataclass
 class Channel:
     name: str
     message_name: str = "a message"
-    message_header: str = "Message from $sender to $receiver:"
+    message_header: str = "Message from $sender to $receivers:"
 
-    def _create_message(self, sender_name, receiver_name, body):
+    def _create_message(self, sender, receivers, body, me):
         header = self.message_header
-        header = header.replace("$sender", sender_name)
-        header = header.replace("$receiver", receiver_name)
+        header = header.replace("$sender", self.make_name(sender, me))
+        header = header.replace(
+            "$receivers", ",".join(self.make_name(r, me) for r in receivers)
+        )
         return header + body
 
+    @staticmethod
+    def make_name(persona, me) -> str:
+        if persona == me:
+            return persona.name + " (me)"
+        else:
+            return persona.name
+
     def query_and_send_message(self, sender, receiver, observers=None):
-        text = sender.query(f"Create {self.message_name} to {receiver.name}.")
-        sender.observe(self._create_message(sender.name + " (me)", receiver.name, text))
-        receiver.observe(
-            self._create_message(sender.name, receiver.name + " (me)", text)
-        )
+        self.query_and_send_message_to_all(sender, [receiver], observers)
+
+    def query_and_send_message_to_all(self, sender, receivers, observers=None):
+        names = ",".join(r.name for r in receivers)
+        text = sender.query(f"Create {self.message_name} to {names}.")
+        sender.observe(self._create_message(sender, receivers, text, sender))
+        for receiver in receivers:
+            receiver.observe(self._create_message(sender, receivers, text, receiver))
         if observers:
             for actor in observers:
-                actor.observe(self._create_message(sender.name, receiver.name, text))
+                actor.observe(self._create_message(sender, receivers, text, actor))
 
 
 ChatChannel = Channel("chat", "a chat message", "Message from $sender to $receiver:")
